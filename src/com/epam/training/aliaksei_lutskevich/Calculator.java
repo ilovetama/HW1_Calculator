3ded761481384731a3e6b77c002ffbbb66e0fd6e
Aliaksei Lutskevich <lutskevich.alexey@gmail.com>
2022-01-17 13:42:58 +0300

    v 2.2

--- src/com/epam/training/aliaksei_lutskevich/Calculator.java
+++ src/com/epam/training/aliaksei_lutskevich/Calculator.java
@@ -1,82 +1,25 @@
 package com.epam.training.aliaksei_lutskevich;
 
-import java.util.Scanner;
+public class Calculator extends CalculationUtils {
 
-public class Calculator implements ICalculator {
-
-    private int precision = 2;
-
-    public static void main(String[] args) {
-
-        Calculator calculate = new Calculator();
-        double firstNumber = getNumber();
-        double secondNumber = getNumber();
-        char operation = getOperation();
-        double result = calculate.calculation(operation,firstNumber,secondNumber);
-        System.out.println(firstNumber + " " + operation + " " + secondNumber + " = " + result);
-    }
-
-    public static double getNumber () {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number:");
-        if (scanner.hasNextDouble() && scanner.hasNext("^[^\\s]*$"))
-        {
-            return scanner.nextDouble();
-        } else {
-            System.out.println("Number is incorrect. Try again");
-            return getNumber();
-        }
-    }
-
-    public static char getOperation () {
-        System.out.println("Enter the operation (+, -, *, /):");
-        Scanner scanner = new Scanner(System.in);
-        char operation;
-        if (scanner.hasNext("[+*/-]")) {
-            operation = scanner.next().charAt(0);
-        } else {
-            System.out.println("Operation is incorrect. Try again");
-            operation = getOperation();
-        }
-        return operation;
-    }
-
-    public double calculation(char operation, double firstNumber, double secondNumber) {
-        Calculator calculate = new Calculator();
-        return switch (operation) {
-            case '+' -> calculate.add(firstNumber, secondNumber);
-            case '-' -> calculate.subtract(firstNumber, secondNumber);
-            case '*' -> calculate.multiply(firstNumber, secondNumber);
-            case '/' -> calculate.divide(firstNumber, secondNumber);
-            default -> 0;
-        };
-    }
-
-    @Override
     public double add(double a, double b) {
         return getResult(a + b);
     }
 
-    @Override
     public double subtract(double a, double b) {
         return getResult(a - b);
     }
 
-    @Override
     public double multiply(double a, double b) {
         return getResult(a * b);
     }
 
-    @Override
     public double divide(double a, double b) {
-        if (b == 0){
+        if (b == 0) {
             System.out.println("Division by zero is prohibited");
             System.exit(0);
         }
         return getResult(a / b);
     }
 
-    public double getResult(double result){
-        return Math.round((result)*(int)Math.pow(10.0, precision))/ Math.pow(10.0, precision);
-    }
 }
---
+++ src/com/epam/training/aliaksei_lutskevich/CalculatorStarter.java
@@ -0,0 +1,12 @@
+package com.epam.training.aliaksei_lutskevich;
+
+public class CalculatorStarter extends CalculationUtils {
+    public static void main(String[] args) {
+        Calculator calculate = new Calculator();
+        double firstNumber = getNumber();
+        double secondNumber = getNumber();
+        char operation = getOperation();
+        double result = calculate.calculation(operation, firstNumber, secondNumber);
+        System.out.println(firstNumber + " " + operation + " " + secondNumber + " = " + result);
+    }
+}
