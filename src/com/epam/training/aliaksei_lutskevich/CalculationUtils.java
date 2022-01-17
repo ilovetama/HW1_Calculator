aed1240ca85f930cfdbf26ab2e1f8617f6e2868f
Aliaksei Lutskevich <lutskevich.alexey@gmail.com>
2022-01-17 15:05:35 +0300

    v 2.21 Google style

--- src/com/epam/training/aliaksei_lutskevich/CalculationUtils.java
+++ src/com/epam/training/aliaksei_lutskevich/CalculationUtils.java
@@ -3,51 +3,53 @@ package com.epam.training.aliaksei_lutskevich;
 import java.util.Scanner;
 
 public class CalculationUtils {
-    private static final String NON_SPACE_REGEX = "^[\\S]*$";
-    private static final String SPACE_REGEX = "^[\\s]*$";
-    private static final String ARITHMETIC_SINGS = "[-*+/]";
 
-    public static double getNumber() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number:");
-        if (scanner.hasNextDouble() && scanner.hasNext(NON_SPACE_REGEX)) {
-            return scanner.nextDouble();
-        } else if (scanner.hasNext(SPACE_REGEX)) {
-            System.out.println("Number is incorrect. Try again");
-            scanner.next();
-            return getNumber();
-        } else {
-            System.out.println("Number is incorrect. Try again");
-            return getNumber();
-        }
-    }
+  private static final String NON_SPACE_REGEX = "^[\\S]*$";
+  private static final String SPACE_REGEX = "^[\\s]*$";
+  private static final String ARITHMETIC_SINGS = "[-*+/]";
 
-    public static char getOperation() {
-        System.out.println("Enter the operation (+, -, *, /):");
-        Scanner scanner = new Scanner(System.in);
-        char operation;
-        if (scanner.hasNext(ARITHMETIC_SINGS)) {
-            operation = scanner.next().charAt(0);
-        } else {
-            System.out.println("Operation is incorrect. Try again");
-            operation = getOperation();
-        }
-        return operation;
+  public static double getNumber() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the number:");
+    if (scanner.hasNextDouble() && scanner.hasNext(NON_SPACE_REGEX)) {
+      return scanner.nextDouble();
+    } else if (scanner.hasNext(SPACE_REGEX)) {
+      System.out.println("Number is incorrect. Try again");
+      scanner.next();
+      return getNumber();
+    } else {
+      System.out.println("Number is incorrect. Try again");
+      return getNumber();
     }
+  }
 
-    public double calculation(char operation, double firstNumber, double secondNumber) {
-        Calculator calculate = new Calculator();
-        return switch (operation) {
-            case '+' -> calculate.add(firstNumber, secondNumber);
-            case '-' -> calculate.subtract(firstNumber, secondNumber);
-            case '*' -> calculate.multiply(firstNumber, secondNumber);
-            case '/' -> calculate.divide(firstNumber, secondNumber);
-            default -> 0;
-        };
+  public static char getOperation() {
+    System.out.println("Enter the operation (+, -, *, /):");
+    Scanner scanner = new Scanner(System.in);
+    char operation;
+    if (scanner.hasNext(ARITHMETIC_SINGS)) {
+      operation = scanner.next().charAt(0);
+    } else {
+      System.out.println("Operation is incorrect. Try again");
+      operation = getOperation();
     }
+    return operation;
+  }
 
-    public double getResult(double result) {
-        final int CALCULATION_ACCURACY = 2; //A quantity of digits after comma
-        return Math.round((result) * (int) Math.pow(10.0, CALCULATION_ACCURACY)) / Math.pow(10.0, CALCULATION_ACCURACY);
-    }
+  public double calculation(char operation, double firstNumber, double secondNumber) {
+    Calculator calculate = new Calculator();
+    return switch (operation) {
+      case '+' -> calculate.add(firstNumber, secondNumber);
+      case '-' -> calculate.subtract(firstNumber, secondNumber);
+      case '*' -> calculate.multiply(firstNumber, secondNumber);
+      case '/' -> calculate.divide(firstNumber, secondNumber);
+      default -> 0;
+    };
+  }
+
+  public double getResult(double result) {
+    final int CALCULATION_ACCURACY = 2; //A quantity of digits after comma
+    return Math.round((result) * (int) Math.pow(10.0, CALCULATION_ACCURACY)) / Math.pow(10.0,
+        CALCULATION_ACCURACY);
+  }
 }
--- src/com/epam/training/aliaksei_lutskevich/Calculator.java
+++ src/com/epam/training/aliaksei_lutskevich/Calculator.java
@@ -2,24 +2,24 @@ package com.epam.training.aliaksei_lutskevich;
 
 public class Calculator extends CalculationUtils {
 
-    public double add(double a, double b) {
-        return getResult(a + b);
-    }
+  public double add(double a, double b) {
+    return getResult(a + b);
+  }
 
-    public double subtract(double a, double b) {
-        return getResult(a - b);
-    }
+  public double subtract(double a, double b) {
+    return getResult(a - b);
+  }
 
-    public double multiply(double a, double b) {
-        return getResult(a * b);
-    }
+  public double multiply(double a, double b) {
+    return getResult(a * b);
+  }
 
-    public double divide(double a, double b) {
-        if (b == 0) {
-            System.out.println("Division by zero is prohibited");
-            System.exit(0);
-        }
-        return getResult(a / b);
+  public double divide(double a, double b) {
+    if (b == 0) {
+      System.out.println("Division by zero is prohibited");
+      System.exit(0);
     }
+    return getResult(a / b);
+  }
 
 }
